3d5bf123cfc66a83d0561305cc3fa6a159cb273d
Kishan Bheemajiyani <kishan.bheemajiyani@gmail.com>
2021-03-20 22:44:51 -0400

    Updated Changes

--- src/com/krishnas/CountingValleys/RepatedStringProblem.java
+++ src/com/krishnas/CountingValleys/RepatedStringProblem.java
@@ -1,10 +1,6 @@
 package com.krishnas.CountingValleys;
 
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class RepatedStringProblem {
@@ -15,6 +11,8 @@ public class RepatedStringProblem {
 		try {
 			long numOfS = n / s.length();
 			long rest = n % s.length();
+			System.out.println(numOfS);
+			System.out.println(rest);
 
 			if (!s.contains("a")) {
 				return 0;
@@ -31,8 +29,9 @@ public class RepatedStringProblem {
 	private static long aCounter(String s, long end) {
 		int a = 0;
 		for (int i = 0; i < end; i++) {
-			if (s.charAt(i) == 'a')
+			if (s.charAt(i) == 'a') {
 				a++;
+			}
 		}
 		return a;
 
@@ -45,7 +44,7 @@ public class RepatedStringProblem {
 		 //long n = scanner.nextLong();
 		//scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-		long result = repeatedString("ab", 10);
+		long result = repeatedString("ab", 9);
 		System.out.println(result);
 
 		//bufferedWriter.write(String.valueOf(result));
